6266e13b7e4582bd85adec6d70728835d0fc4e97
Nik <snive2013@yandex.ru>
2018-08-26 02:01:59 +0300

    A bunch of small api views have been replaced by single viewset

--- backend/ippo_tutor/apps/authentication/urls.py
+++ backend/ippo_tutor/apps/authentication/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
 from rest_auth.views import LoginView, LogoutView
 
-from .views import UserRetrieveAndPasswordChangeAPIView, UserListCreateAPIView, UserDeleteAPIView
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register('', UserViewSet)
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
-
-    path('<str:username>/delete/', UserDeleteAPIView.as_view()),
-    path('<str:username>/', UserRetrieveAndPasswordChangeAPIView.as_view()),
-    path('', UserListCreateAPIView.as_view()),
 ]
+
+urlpatterns += router.urls
 
--- backend/ippo_tutor/apps/authentication/views.py
+++ backend/ippo_tutor/apps/authentication/views.py
@@ -1,57 +1,53 @@
-import django.db
-
-from rest_framework import status, generics
+from rest_framework import status, generics, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.exceptions import ValidationError
 
 from ippo_tutor.apps.core.permissions import IsTutorOrTargetUser, IsTutor
-from ippo_tutor.apps.core.helpers import DRFViewPermissionsSubstituteManager, DRFViewSerializerSubstituteManager
 
 from .models import User
 from .serializers import UserSerializer, ChangeUserPasswordSerializer, CreateUserSerializer
 
 
-class UserRetrieveAndPasswordChangeAPIView(generics.RetrieveUpdateAPIView):
-    permission_classes = (IsAuthenticated, IsTutorOrTargetUser,)
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     lookup_field = 'username'
 
-    def retrieve(self, request, *args, **kwargs):
-        with DRFViewSerializerSubstituteManager(self, attr_value=UserSerializer):
-            return super().retrieve(request, *args, **kwargs)
+    def get_permissions(self):
+        permission_classes = self.permission_classes[:]
+        if self.action == 'list':
+            permission_classes += [IsTutor]
+        elif self.action in ['create', 'delete']:
+            permission_classes += [IsAdminUser]
+        else:
+            permission_classes += [IsTutorOrTargetUser]
+
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        print('action: ', self.action)
+        if self.action in ['retrieve', 'list']:
+            return UserSerializer
+        elif self.action == 'update':
+            return ChangeUserPasswordSerializer
+        else:
+            return CreateUserSerializer
 
     def update(self, request, *args, **kwargs):
-        with DRFViewSerializerSubstituteManager(self, attr_value=ChangeUserPasswordSerializer):
-            user = self.get_object()
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
+        user = self.get_object()
+        serializer = self.get_serializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
 
-            if not user.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            user.set_password(serializer.data.get("new_password"))
-            user.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        if not user.check_password(serializer.data.get("old_password")):
+            return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
 
+        user.set_password(serializer.data.get("new_password"))
+        user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-class UserListCreateAPIView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
-    queryset = User.objects.all()
+    def perform_create(self, serializer):
+        if User.objects.filter(username=self.request.data.get('username')).exists():
+            raise ValidationError('User already exists.')
 
-    def list(self, request, *args, **kwargs):
-        with DRFViewSerializerSubstituteManager(self, attr_value=UserSerializer):
-            with DRFViewPermissionsSubstituteManager(self, attr_value=(IsTutor,)):
-                return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        with DRFViewSerializerSubstituteManager(self, attr_value=CreateUserSerializer):
-            with DRFViewPermissionsSubstituteManager(self, attr_value=(IsAdminUser,)):
-                if User.objects.filter(username=request.data.get('username', None)).exists():
-                    return Response({"detail": ["User already exists."]}, status=status.HTTP_400_BAD_REQUEST)
-                else:
-                    return super().create(request, *args, **kwargs)
-
-
-class UserDeleteAPIView(generics.DestroyAPIView):
-    permission_classes = (IsAuthenticated, IsAdminUser)
-    queryset = User.objects.all()
-    lookup_field = 'username'
+        User.objects.create_user(**serializer.validated_data)
